62760776005f5f9b158afa89ebb3b5f2e23b4e3f
Fersanseg <fer_ssa94@hotmail.com>
2024-09-22 20:45:50 +0200

    Added css stylesheet

--- src/main/java/com/actt/actt/MainApplication.java
+++ src/main/java/com/actt/actt/MainApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
 
@@ -16,7 +17,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load()/*, 1600, 800*/);
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add("styles.css");
         stage.setTitle("Assetto Corsa Tournament Tracker");
         stage.setScene(scene);
         stage.show();
