28ef5e8b795a56d754e7d3ed7ca56a1f71443ee6
Fersanseg <fer_ssa94@hotmail.com>
2024-09-23 21:40:46 +0200

    Check OS and close if not supported

--- src/main/java/com/actt/actt/MainController.java
+++ src/main/java/com/actt/actt/MainController.java
@@ -6,9 +6,13 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -45,5 +49,34 @@ public class MainController implements Initializable {
         tournamentsComboBox.setItems(tournamentsList);
         tournamentsComboBox.setPrefWidth(600);
         tournamentsComboBox.setPrefHeight(50);
+
+        checkAppConfig();
+    }
+
+    public void checkAppConfig() {
+        String os = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+        if (os.contains("win")) {
+            // check config on AppData/Local
+        }
+        else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            // TODO check config on linux OS
+        }
+        else {
+            showInvalidOSError();
+        }
+    }
+
+    private void showInvalidOSError() {
+        ButtonType closeButton = new ButtonType("Understood", ButtonBar.ButtonData.OK_DONE);
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Unsupported OS");
+        dialog.setContentText("Your Operating System is not currently supported!\nThe app only supports Windows at " +
+                "this time. Support for other OSs will come soon!");
+        dialog.getDialogPane().getButtonTypes().add(closeButton);
+        dialog.showAndWait().ifPresent(r -> {
+            if (r.getButtonData() == ButtonBar.ButtonData.OK_DONE) {
+                System.exit(1);
+            }
+        });
     }
 }
