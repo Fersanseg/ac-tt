ef938b290e3de89a9708f46fca6a7bac7ffe0cee
Fersanseg <fer_ssa94@hotmail.com>
2024-09-24 19:23:28 +0200

    Initial config dialog layout

--- src/main/java/com/actt/actt/fileOperations/FileOperations.java
+++ src/main/java/com/actt/actt/fileOperations/FileOperations.java
@@ -3,6 +3,10 @@ package com.actt.actt.fileOperations;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -35,14 +39,30 @@ public class FileOperations {
     }
 
     private static void showInitialConfigDialog() {
+        double dialogWidth = 700;
         ButtonType closeButton = new ButtonType("Go", ButtonBar.ButtonData.OK_DONE);
         Dialog<ButtonType> dialog = new Dialog<>();
         dialog.setTitle("Configuration");
-        dialog.setContentText("It seems like this is the first time you launch the app. Welcome!\n\nPlease introduce " +
-                "your Assetto Corsa installation path (usually located in your Steam folder -> " +
-                "steamapps -> common -> assettocorsa), as well as the folder where you want to save your tournaments.");
         dialog.getDialogPane().getButtonTypes().add(closeButton);
-        dialog.getDialogPane().setPrefWidth(800);
+        dialog.getDialogPane().setPrefWidth(dialogWidth);
+
+        /*
+        DirectoryChooser filePicker = new DirectoryChooser();
+        File folder = filePicker.showDialog(dialog.getOwner());*/
+
+        //noinspection SpellCheckingInspection
+        Text mainText = new Text("""
+                It seems like this is the first time you launch the app. Welcome!
+                
+                Please introduce your Assetto Corsa installation path (usually located in your Steam folder -> \
+                steamapps -> common -> assettocorsa), as well as the folder where you want to save your tournaments.""");
+        mainText.setWrappingWidth(dialogWidth);
+        mainText.setFont(new Font(16));
+        TextField acPathField = new TextField();
+        TextField appPathField = new TextField();
+        VBox container = new VBox(20.0, mainText, acPathField, appPathField);
+        dialog.getDialogPane().setContent(container);
+
         dialog.showAndWait();
     }
 
@@ -53,8 +73,6 @@ public class FileOperations {
         dialog.setContentText("Your Operating System is not currently supported!\nThe app only supports Windows at " +
                 "this time. Support for other OSs will come soon!");
         dialog.getDialogPane().getButtonTypes().add(closeButton);
-        dialog.showAndWait().ifPresent(_ -> {
-                System.exit(1);
-        });
+        dialog.showAndWait().ifPresent(_ -> System.exit(1));
     }
 }
